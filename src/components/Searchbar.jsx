035016e72ce3badd7f4ff201038cccc912aea491
RajDeft <bhothardstudy@gmail.com>
2026-07-29 16:43:24 +0530

    V4 Added genre filter functionalities, and search bar ui

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -8,8 +8,8 @@ const Searchbar = () => (
    <label htmlFor='search-field' className='sr-only'>
     Search Songs
    </label>
-   <div className='flex flex-row justify-start items-cente'>
-      <FiSearch className='w-5 h-5 ml-4'>
+   <div className='flex flex-row justify-start items-center'>
+      <FiSearch className='w-5 h-5 ml-4'/>
         <input
          name='search-field'
          autoComplete='off'
@@ -19,9 +19,9 @@ const Searchbar = () => (
          value=''
          onChange={()=>{}}
          className='flex-1 bg-transparent border-none 
-          outline-none placeholder-gray-500 text-base text-white p-4'
+          outline-none placeholder-gray-500 text-base
+           text-white p-4'
          />
-      </FiSearch>
    </div>
 
   </form>
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,54 +1,79 @@
 import {useDispatch, useSelector } from 'react-redux';
+import { useState } from "react";
 
 import { Error, Loader, SongCard, } from "../components";
-import { genres } from "../assets/constants";
+
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import { adaptAudiusSong } from "../utils/audiusAdapter";
 
+
+
 const Discover = () => {
     const dispatch = useDispatch();
-    const { activeSong, isPlaying } = useSelector((state)=> state.player);
+    const { activeSong, isPlaying, genreListId } = useSelector((state)=> state.player);
     const {data, isFetching, error} = useGetTopChartsQuery();
-    const genreTitle = 'Pop';
+    const [selectedGenre, setSelectedGenre] = useState("All");
+    const genreTitle = selectedGenre;
+
+    const songs = data?.data?.map(adaptAudiusSong) ?? [];
+
+    
+    const genreOptions = ["All", ...new Set( songs
+            .map((song) => song.genre)
+            .filter(Boolean)),];
+            
+    const filteredSongs = selectedGenre === "All" ? songs
+          : songs.filter((song) =>
+          song.genre?.toLowerCase() === selectedGenre.toLowerCase());
+    
+
     // const test= data?.data;
     // console.log(test);
     
-    const songs = data?.data?.map(adaptAudiusSong) ?? [];
+    
     
     if(isFetching) return <Loader title= "Loading songs..."/>;
 
     if(error) return <Error/>;
     
-    return(
-    <div className="flex flex-col">
-        <div className="w-full flex justify-between items-center 
+    return (
+      <div className="flex flex-col">
+        <div
+          className="w-full flex justify-between items-center 
          sm:flex-row flex-col mt-4 mb-10">
-            <h2 className="font-bold text-3xl text-white 
-            text-left">Discover {genreTitle}</h2>
-            <select 
-            onChange={()=>{}}
-            value=""
-            className="bg-black text-grey-300 p-3 text-sm rounded-lg outline-none
-            sm:mt-0 mt-5">
-            {genres.map((genre) => <option key={genre.value}
-            value={genre.value}>{genre.title}</option>)}
-            </select>  
+          <h2
+            className="font-bold text-3xl text-white 
+            text-left">
+            Discover {genreTitle}
+          </h2>
+
+          <select
+            value={selectedGenre}
+            onChange={(e) => setSelectedGenre(e.target.value)}
+            className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none">
+            {genreOptions.map((genre) => (
+              <option key={genre} value={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
+
         </div>
 
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-            {songs.map((song,i)=>(
-                <SongCard
-                  key={song?.key}
-                  song={song}
-                  isPlaying = {isPlaying}
-                  activeSong = {activeSong}
-                  data = {songs}
-                  i={i}
-                />
-            ))}
+          {filteredSongs.map((song, i) => (
+            <SongCard
+              key={song?.key}
+              song={song}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              data={filteredSongs}
+              i={i}
+            />
+          ))}
         </div>
-    </div>
-   );
+      </div>
+    );
 
 };
 export default Discover;
